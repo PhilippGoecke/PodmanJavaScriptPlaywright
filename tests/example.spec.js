a54bb3e9abde42ac0862e3cb163618e8db889a40
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-31 20:18:08 +0100

    Add click and text expectation to basic test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,4 +3,6 @@ const { test, expect } = require('@playwright/test');
 test('basic test', async ({ page }) => {
   await page.goto('https://nodejs.org/');
   await expect(page).toHaveTitle(/Node.js — Run JavaScript Everywhere/);
+  await page.click('text=Get Node.js®');
+  await expect(page).toHaveText(/Download Node.js®/);
 });
